408ec6d0b73cdbaa6dee9292e1b75437ab815c8a
flipzky1337 <rorytheloli1337@gmail.com>
2025-03-01 16:02:37 +0500

    refactor AdminPanel; implement user data fetching and display in DataTable component

---
+++ src/components/pages/admin/ui/user/columns.tsx
@@ -0,0 +1,60 @@
+import {ColumnDef} from "@tanstack/react-table";
+import {User} from "@/components/pages/admin/api/type.ts";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuLabel
+} from "@/components/ui/dropdown-menu.tsx";
+import {Menu} from "lucide-react";
+
+export const userColumns: ColumnDef<User>[] = [
+  {
+    header: 'Name',
+    accessorKey: 'name'
+  },
+  {
+    header: 'Email',
+    accessorKey: 'email'
+  },
+  {
+    header: 'Surname',
+    accessorKey: 'surname'
+  },
+  {
+    header: 'Firstname',
+    accessorKey: 'firstname'
+  },
+  {
+    header: 'Middlename',
+    accessorKey: 'middlename'
+  },
+  {
+    header: 'Roles',
+    accessorKey: 'roles',
+  },
+  {
+    id: 'userActions',
+    cell: ({row}) => {
+      const user = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Menu/>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              Actions
+            </DropdownMenuLabel>
+            <DropdownMenuItem>
+              View additional user details
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+      )
+    }
+  }
+]
